7947efc1408a1444a44a2d721bcb6c824a8dfb02
udit <yudistrange@gmail.com>
2019-08-23 10:26:04 +0530

    Add API to peel oranges

--- lib/exkubed_web/router.ex
+++ lib/exkubed_web/router.ex
@@ -1,11 +1,15 @@
 defmodule ExkubedWeb.Router do
   use ExkubedWeb, :router
 
+  alias ExkubedWeb.Controllers.Orange
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/api", ExkubedWeb do
+  scope "/" do
     pipe_through :api
+
+    get("/peel", Orange, :peel)
   end
 end
---
+++ test/exkubed_web/controllers/orage_test.exs
@@ -0,0 +1,11 @@
+defmodule ExkubedWeb.Controllers.OrangeTest do
+  use ExkubedWeb.ConnCase, async: true
+
+  alias ExkubedWeb.Controllers.Orange
+
+  test "Should peel an orange", %{conn: conn} do
+    resp = Orange.peel(conn, %{})
+    assert 200 == resp |> Map.get(:status)
+    assert String.contains? resp |> Map.get(:resp_body), "Peeling an orange for you!"
+  end
+end
