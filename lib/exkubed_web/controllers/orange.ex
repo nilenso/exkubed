8902c87a7edd60e703a05a8abbd0ed0cf438e18b
udit <yudistrange@gmail.com>
2019-08-26 17:56:22 +0530

    Formatting changes

--- lib/exkubed_web/controllers/orange.ex
+++ lib/exkubed_web/controllers/orange.ex
@@ -2,6 +2,7 @@ defmodule ExkubedWeb.Controllers.Orange do
   use ExkubedWeb, :controller
 
   def peel(conn, _params) do
-    json(conn, %{message: "Peeling an orange for you! #{inspect self()}"})
+    json(conn, %{message: "Peeling an orange for you! #{inspect(self())}"})
+  end
   end
 end
--- test/exkubed_web/controllers/orage_test.exs
+++ test/exkubed_web/controllers/orage_test.exs
@@ -6,6 +6,6 @@ defmodule ExkubedWeb.Controllers.OrangeTest do
   test "Should peel an orange", %{conn: conn} do
     resp = Orange.peel(conn, %{})
     assert 200 == resp |> Map.get(:status)
-    assert String.contains? resp |> Map.get(:resp_body), "Peeling an orange for you!"
+    assert String.contains?(resp |> Map.get(:resp_body), "Peeling an orange for you!")
   end
 end
